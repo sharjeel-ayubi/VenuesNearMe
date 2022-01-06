22272844e1c838f35dc56f40a7af1c4f726c8667
Sharjeel Ayubi <sharjeel.ayubi.007@gmail.com>
2022-01-06 21:54:54 +0500

    Initial Commit

---
+++ VenuesNearMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VenuesNearMe
+//
+//  Created by Ayubi on 1/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
